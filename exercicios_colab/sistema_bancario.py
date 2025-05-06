aa189d3e0aca7fe0a78badace1733583e42823e3
debysouza <debsdebbie90@gmail.com>
2025-05-06 18:21:54 -0300

    update

---
+++ exercicios_colab/sistema_bancario.py
@@ -0,0 +1,12 @@
+# 1- Criar lógica que verifica o saldo de
+# uma conta corrente.
+# - conta1, saldo
+
+# 2- Criar uma segunda conta corrente, para simular
+# saque, depósito e transferência.
+# - conta2, saldo
+# - saque, depósito e transferência
+
+# 3- Criar um menu que permite ao usuário realizar N
+# transações até que ele opte por sair.
+# - utilizar while com menu
